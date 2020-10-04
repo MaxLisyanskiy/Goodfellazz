220db33cb76f27ae76ea56eadb710931891874a7
Max <max_lisyanskiy@mail.ru>
2020-10-04 16:32:46 +0300

    finish

--- app/js/app.js
+++ app/js/app.js
@@ -1,5 +1,26 @@
-document.addEventListener("DOMContentLoaded", function() {
+$(document).ready(function() {
+		$('.toggle-hamburger__animx').on('click', function(){
+			$('.toggle-hamburger__animx').toggleClass('is-active');
+			if( $(this).hasClass('is-active')){
+				$('.header__menu_hide').fadeIn('slow');
+			} else{
+				$('.header__menu_hide').fadeOut('slow');
+			}
+		});
 
-	// Custom JS
+		$(".owl-carousel").owlCarousel({
+			items: 1,
+			loop: true,
+			dots: false,
+			responsive: {
+				0: {
+					nav:false
+				},
+				768:{
+					nav: true,
+					navText: ["<img src=\"images/dest/arrow-left.png\">","<img src=\"images/dest/arrow-right.png\">"]
+				}
+			}
+		});
 
 });
